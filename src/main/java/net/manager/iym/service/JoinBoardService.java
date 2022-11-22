9fbdcd27419c79acff01c78268f9e35c8d634fa5
JoonYongBAE <mn1206@naver.com>
2022-11-23 00:33:58 +0900

    test

--- src/main/java/net/manager/iym/service/JoinBoardService.java
+++ src/main/java/net/manager/iym/service/JoinBoardService.java
@@ -25,7 +25,7 @@ public class JoinBoardService {
                 .collect(Collectors.toList());
         return allList;
         //매퍼를 통해서 findAll()을 통해서 나온 정보들은 각각 다른 TodoVO들이기 때문에
-        // 이 각각의 정보들을 vo를 DTO로 변환을 시켜 묶어준다.
+        // 이 각각의 정보들을 vo를 DTO로 변환시켜 묶어준다.
         //List<TodoVO>를 List<TodoDTO>로 변환하는 작업을 stream을 이용하여 각 TodoVO는
         // map()을 통해서 TodoDTO로 바꾸고 collect()을 이용하여 List<TodoDTO>로 묶어준다.
     }
