3ada6d8d661935d439a30179ccb6b52d9ca63dc1
sojin91 <sojin91>
2022-12-02 10:45:21 +0900

    12.02

--- src/main/java/net/manager/iym/controller/HomeController.java
+++ src/main/java/net/manager/iym/controller/HomeController.java
@@ -2,11 +2,12 @@ package net.manager.iym.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping(value = "/")
     public String index() {
-        return "index";
+        return "first1";
     }
 }
