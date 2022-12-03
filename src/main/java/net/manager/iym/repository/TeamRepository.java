603f7a8897c67d298cd9b58e9cb7d144f38c0ba1
JoonYongBAE <mn1206@naver.com>
2022-12-03 20:54:50 +0900

    팀 강제 주입 테스트 성공

--- src/main/java/net/manager/iym/repository/TeamRepository.java
+++ src/main/java/net/manager/iym/repository/TeamRepository.java
@@ -3,6 +3,11 @@ package net.manager.iym.repository;
 import net.manager.iym.domain.JoinBoard;
 import net.manager.iym.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface TeamRepository extends JpaRepository<Team, String> {
+import java.util.Optional;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+    @Query("select b from Team b where b.teamNum =:teamNum")
+    Optional<Team> findById(Long teamNum);
 }
