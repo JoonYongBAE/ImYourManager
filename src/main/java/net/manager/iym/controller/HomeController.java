3b3ad90bd80ac3fbb593153cf6850d05748d99f0
JoonYongBAE <mn1206@naver.com>
2022-12-03 17:37:28 +0900

    22.12.03 17:00

--- src/main/java/net/manager/iym/controller/HomeController.java
+++ src/main/java/net/manager/iym/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/main")
-public class HomeController {
+public class HomeController {//
     @GetMapping("/home")
     public String home() {
         return "home";
